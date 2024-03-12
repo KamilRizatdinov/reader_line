81407acc28e48788f99db73a7a6a779aa9c081de
KamilRizatdinov <k.rizatdinov@innopolis.university>
2024-03-13 01:38:27 +0300

    feat: open welcome page

---
+++ public/background.js
@@ -0,0 +1,13 @@
+chrome.runtime.onInstalled.addListener((details) => { 
+  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    chrome.tabs.create({
+      url: "https://kamilrizatdinov.github.io/reader_line/",
+    });
+  } else if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) { 
+    // When extension is updated
+  } else if ( details.reason === chrome.runtime.OnInstalledReason.CHROME_UPDATE) {
+    // When browser is updated
+  } else if (details.reason === chrome.runtime.OnInstalledReason.SHARED_MODULE_UPDATE) {
+    // When a shared module is updated
+  }
+});
