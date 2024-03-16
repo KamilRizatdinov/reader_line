ddfcd39205bbc8530a31e1544a42918ac227a802
KamilRizatdinov <k.rizatdinov@innopolis.university>
2024-03-16 12:09:19 +0300

    fix: review

--- src/components/RateUs/index.jsx
+++ src/components/RateUs/index.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useCallback} from 'react';
 import {StarFilled, StarOutlined} from '@ant-design/icons';
-import {Flex} from 'antd';
+import {Flex, Divider} from 'antd';
 
 import './RateUs.css';
 
@@ -15,14 +15,29 @@ const RateUs = () => {
   }, []);
 
   return (
-    <Flex gap="4px" className='rate-us' onMouseLeave={() => setRating(0)}>
-      {[1,2,3,4,5].map(i => {
-        if (i <= rating) {
+    <div className='rate-us'>
+      <Divider>Rate us:</Divider>
+      <Flex gap="4px" className='rate-us__stars' onMouseLeave={() => setRating(0)}>
+        {[1,2,3,4,5].map(i => {
+          if (i <= rating) {
+            return (
+              <StarFilled 
+                key={i}
+                onMouseEnter={() => setRating(i)} 
+                onClickCapture={() => onClick(rating)}
+                style={{
+                  fontSize: "30px", 
+                  color: "#1D8B70",
+                  cursor: "pointer",
+                }} 
+              />
+            )
+          }
+
           return (
-            <StarFilled 
-              key={i}
+            <StarOutlined 
+              key={i}  
               onMouseEnter={() => setRating(i)} 
-              onClickCapture={() => onClick(rating)}
               style={{
                 fontSize: "30px", 
                 color: "#1D8B70",
@@ -30,21 +45,9 @@ const RateUs = () => {
               }} 
             />
           )
-        }
-
-        return (
-          <StarOutlined 
-            key={i}  
-            onMouseEnter={() => setRating(i)} 
-            style={{
-              fontSize: "30px", 
-              color: "#1D8B70",
-              cursor: "pointer",
-            }} 
-          />
-        )
-      })}
-    </Flex>
+        })}
+      </Flex>
+    </div>
   );
 };
 
